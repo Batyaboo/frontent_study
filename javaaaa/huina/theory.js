21d026e557f95d05472519d0e3516c86f66945f4
Batyrhan Khamitov <batyrhanhamitov@gmail.com>
2025-05-06 23:52:32 +0300

    obnova11111

---
+++ javaaaa/huina/theory.js
@@ -0,0 +1,33 @@
+// operators
+
+let a = 10;
+let b = 5;
+let c = 32;
+
+// c = c + a;
+// c = c - a;
+// c = c * a;
+// c = c / a;
+
+// c += a;
+
+// c = 4 ** 3;
+
+console.log(c);
+
+//====================
+//data types
+
+const age = 29; //number
+const name = "Batyrhan"; //string
+const isProgrammer = true; //boolean
+let x; //undefined
+null; //null
+
+//========================== Priority
+const fullAge = 29;
+const birthYear = 2003;
+const currentYear = 2025;
+// < > >= <=
+
+// ===  тройное равно проверяет не только значение но и тип данных
